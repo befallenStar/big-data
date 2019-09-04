e2423fcb01b259cfa0148a44c11571f0908a486b
Junenatte-and-little-pig <sy5622_5@126.com>
2019-09-04 08:31:33 +0800

    Initial commit

---
+++ hadoop/src/main/java/com/junenatte/hadoop/predemo/WordCountMapper.java
@@ -0,0 +1,21 @@
+package com.junenatte.hadoop.predemo;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class WordCountMapper extends Mapper<LongWritable, Text,Text, IntWritable> {
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String line=value.toString();
+        StringTokenizer stringTokenizer=new StringTokenizer(line);
+        while(stringTokenizer.hasMoreElements()){
+            String word=(String)stringTokenizer.nextElement();
+            context.write(new Text(word),new IntWritable(1));
+        }
+    }
+}
